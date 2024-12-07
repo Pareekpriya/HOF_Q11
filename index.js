25b99999b6cada92f839df99c4ec4d8123e5de5f
Pareekpriya <priyapareek754@gmail.com>
2024-12-07 19:30:32 +0530

    HOF_Q11

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let product = [{name:"Laptop",price:9000},{name:"Mouse",price:40},{name:"Keyboard",price:500}];
 
-let processProducts = product.map(productNames => product.name);
+let processProducts = product.map(item => item.name);
 
 console.log(processProducts);
 
-processProducts.forEach(productNames => product.price>50? 'product.name is above $50':'product.name is below $50')
+product.forEach((_, index) => {
+    let item = product[index];
+    console.log(item.price >50 ? `${item.name} is above $50` : `${item.name} is below $50`);
+    });
